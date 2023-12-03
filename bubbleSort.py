21c90940ee6700dcec5f18cebae5e294f7f30bf8
libaishwarya <libaishwarya@gmail.com>
2023-12-03 05:55:09 +0530

    updated bubble sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,7 +1,8 @@
-arr = [2,1,5,3]
-for i in range(len(arr)-1):
-    if arr[i] > arr[i+1]:
-        temp = arr[i+1]
-        arr[i+1] = arr[i]
-        arr[i] = temp
+arr= [64, 34, 25, 12, 22, 11, 90]
+for i in range(len(arr)):
+    for j in range (len(arr)-i-1):
+        if (arr[j] > arr[j+1]):
+            temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
 print(arr)
